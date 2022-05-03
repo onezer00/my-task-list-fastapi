fec3d2fd24d0ac06aa8ec48017504b55049b878c
onezer00 <joao.batista@spassu.com-at-702413981770>
2022-05-03 18:11:17 -0300

    verify method get using SDK deta

--- main.py
+++ main.py
@@ -3,79 +3,58 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from deta import Deta
 
-import datetime
-import load_envs
-"""
-It is responsible for the configuration
-
-...
-
-Methods
--------
-get_current_config()
-    Gets current config
-"""
-
-from os import getenv
-
-load_envs.load()
-
-configuration = {
-    "title":"Python Task List",
-    "version":getenv("VERSION", "FALILED TO LOAD VERSION"),
-    "author":"Oner",
-    "description":'A simple task list with fastapi',
-    "python_requires":">:3.5",
-    "last_updated": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "SQLALCHEMY_DATABASE_URI": "postgresql://sql10489830:mfpJPYJ71Y@sql10489830:3306/task_app",
-}
-
 deta = Deta()
 
-tasks = deta.Base('task-db')
+tasks = deta.Base("task-crud")
 
-app = FastAPI(**configuration)
+app = FastAPI()
 
-class Task(BaseModel):
-    task_name: str
-    task_description: str
-    task_creation: str
+
+class User(BaseModel):
+    name: str
+    age: int
+    hometown: str
+
+
+class UserUpdate(BaseModel):
+    name: str = None
+    age: int = None
+    hometown: str = None
 
 @app.get("/")
-async def read_root():
-    '''
-    Description:
-    -------
-    Async function to describe the name and version of the application
+def read_root():
+    return {"Hello": "There"}
 
-    Returns
-    -------
-    dict
-        Returns a dict with the App Name and version
-    '''    
-    return {"AppName": "FAST LIST WITH PYTHON", "AppVersion": app.version}
-
-
-@app.get("/version")
-async def read_item():
-    '''
-    Description:
-    -------
-    Async function to describe the last update of the application
-
-    Returns
-    -------
-    dict
-        Returns a dict with the app version and last update
-    '''    
-    return {"AppVersion": app.version, "LastUpdated": app.extra["last_updated"]}
 
 @app.post("/tasks", status_code=201)
-def create_user(task: Task):
-    task = tasks.put(task.dict())
-    return task
+def create_user(user: User):
+    u = tasks.put(user.dict())
+    return u
 
 @app.get("/tasks")
-def list_users():
+def list_tasks():
     us = next(tasks.fetch())
     return us
+
+@app.get("/tasks/{uid}")
+def get_user(uid: str):
+    user = tasks.get(uid)
+    if user:
+        return user
+    return JSONResponse({"message": "user not found"}, status_code=404)
+
+
+@app.patch("/tasks/{uid}")
+def update_user(uid: str, uu: UserUpdate):
+    updates = {k:v for k,v in uu.dict().items() if v is not None}
+    try:
+        tasks.update(updates, uid)
+        return tasks.get(uid)
+    except Exception:
+        return JSONResponse({"message": "user not found"}, status_code=404)
+
+
+@app.delete("/tasks/{uid}")
+def delete_user(uid: str):
+    tasks.delete(uid)
+    return
