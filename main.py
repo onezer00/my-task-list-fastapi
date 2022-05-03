5466c15f69e1de7b146bc55a53c62d005ff75d77
onezer00 <joao.batista@spassu.com-at-702413981770>
2022-05-03 19:04:16 -0300

    verify method get using SDK deta

--- main.py
+++ main.py
@@ -3,58 +3,96 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from deta import Deta
 
-deta = Deta()
+from os import getenv
+import datetime
 
-tasks = deta.Base("task-crud")
+deta = Deta()
+task_db = deta.Base("task-crud")
+
+configuration = {
+    "title":"Python Task List",
+    "version":getenv("VERSION", "FALILED TO LOAD VERSION"),
+    "author":"Oner",
+    "description":'A simple task list with fastapi',
+    "python_requires":">:3.5",
+    "last_updated": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+}
 
 app = FastAPI()
 
+class Task(BaseModel):
+    task_name: str
+    task_description: str
+    task_created: str
+    
+class TaskUpdate(BaseModel):
+    task_name: str = None
+    task_description: str = None
 
-class User(BaseModel):
-    name: str
-    age: int
-    hometown: str
+@app.get("/")
+async def read_root():
+    '''
+    Description:
+    -------
+    Async function to describe the name and version of the application
+
+    Returns
+    -------
+    dict
+        Returns a dict with the App Name and version
+    '''    
+    return {"AppName": "FAST LIST WITH PYTHON", "AppVersion": app.version}
 
 
-class UserUpdate(BaseModel):
-    name: str = None
-    age: int = None
-    hometown: str = None
+@app.get("/version")
+async def read_item():
+    '''
+    Description:
+    -------
+    Async function to describe the last update of the application
+
+    Returns
+    -------
+    dict
+        Returns a dict with the app version and last update
+    '''    
+    return {"AppVersion": app.version, "LastUpdated": app.extra["last_updated"]}
 
 @app.get("/")
 def read_root():
-    return {"Hello": "There"}
+    return {"Hello": "There!"}
 
-
-@app.post("/tasks", status_code=201)
-def create_user(user: User):
-    u = tasks.put(user.dict())
-    return u
+@app.post("/tasks")
+def create_task(task: Task):
+    task['task_created'] = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    task_db.put(task.dict())
+    return task_db.fetch()
 
 @app.get("/tasks")
-def list_tasks():
-    us = next(tasks.fetch())
-    return us
+def list_users(limit: int = 100):
+    tasks = task_db.fetch(limit=limit)
+    return tasks.items
 
 @app.get("/tasks/{uid}")
 def get_user(uid: str):
-    user = tasks.get(uid)
-    if user:
-        return user
-    return JSONResponse({"message": "user not found"}, status_code=404)
+    task = task_db.get(uid)
+    if task:
+        return task
+    return JSONResponse({"message": "Task not found"}, status_code=404)
 
 
 @app.patch("/tasks/{uid}")
-def update_user(uid: str, uu: UserUpdate):
+def update_user(uid: str, uu: TaskUpdate):
+    uid['task_created'] = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     updates = {k:v for k,v in uu.dict().items() if v is not None}
     try:
-        tasks.update(updates, uid)
-        return tasks.get(uid)
+        task_db.update(updates, uid)
+        return task_db.get(uid)
     except Exception:
         return JSONResponse({"message": "user not found"}, status_code=404)
 
 
 @app.delete("/tasks/{uid}")
 def delete_user(uid: str):
-    tasks.delete(uid)
+    task_db.delete(uid)
     return
